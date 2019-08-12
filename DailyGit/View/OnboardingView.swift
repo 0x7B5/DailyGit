b7d602049a087bcea981d2e8e52923071f5c7828
Vlad Munteanu <vladcmunteanu@gmail.com>
2019-08-12 01:21:48 -0400

    Finished onboarding screen view.

--- DailyGit/View/OnboardingView.swift
+++ DailyGit/View/OnboardingView.swift
@@ -23,9 +23,11 @@ public class OnboardingView: UIView {
     }
     
     private func initializeUI() {
-       //UIViews
-       addSubview(enterUsernameLabel)
-       addSubview(usernameTextfield)
+        //UIViews
+        addSubview(enterUsernameLabel)
+        addSubview(usernameTextfield)
+        addSubview(dontKnowUsernameLabel)
+        addSubview(githubPhoto)
     }
     
     public func createConstraints() {
@@ -38,18 +40,28 @@ public class OnboardingView: UIView {
         usernameTextfield.snp.makeConstraints {
             $0.width.equalToSuperview().multipliedBy(0.9)
             $0.centerX.equalToSuperview()
-            $0.centerY.equalToSuperview().multipliedBy(0.55)
+            $0.centerY.equalToSuperview().multipliedBy(0.65)
+        }
+        dontKnowUsernameLabel.snp.makeConstraints {
+            $0.width.equalToSuperview().multipliedBy(0.9)
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalToSuperview().multipliedBy(1.0)
+        }
+        githubPhoto.snp.makeConstraints {
+            $0.width.equalToSuperview()
+            $0.centerX.equalToSuperview()
+            $0.centerY.equalToSuperview().multipliedBy(1.5)
         }
     }
     
     //Next Button
-//    let nextButton: UIButton = {
-//        let button = UIButton()
-//        button.setTitle("Next",for: .normal)
-//        button.titleLabel?.font = UIFont.preferredFont(forTextStyle: .subheadline)
-//
-//        return button
-//    }()
+    //    let nextButton: UIButton = {
+    //        let button = UIButton()
+    //        button.setTitle("Next",for: .normal)
+    //        button.titleLabel?.font = UIFont.preferredFont(forTextStyle: .subheadline)
+    //
+    //        return button
+    //    }()
     
     //Enter Github Username
     let enterUsernameLabel: UILabel = {
@@ -60,7 +72,7 @@ public class OnboardingView: UIView {
         label.adjustsFontSizeToFitWidth = true
         label.text = "Enter GitHub Username"
         label.textAlignment = .center
-        label.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        label.textColor = #colorLiteral(red: 0.2352941176, green: 0.2352941176, blue: 0.262745098, alpha: 0.6)
         return label
     }()
     
@@ -69,9 +81,9 @@ public class OnboardingView: UIView {
         let textfield = UITextField()
         let centeredParagraphStyle = NSMutableParagraphStyle()
         centeredParagraphStyle.alignment = .center
-        let attributedPlaceholder = NSAttributedString(string: "Username", attributes: [NSAttributedString.Key.paragraphStyle: centeredParagraphStyle])
+        let attributedPlaceholder = NSAttributedString(string: "Username", attributes: [NSAttributedString.Key.paragraphStyle: centeredParagraphStyle, NSAttributedString.Key.foregroundColor : #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)])
         textfield.attributedPlaceholder = attributedPlaceholder
-        textfield.font = UIFont.systemFont(ofSize: 45, weight: UIFont.Weight(rawValue: 1.0))
+        textfield.font = UIFont.systemFont(ofSize: 42, weight: UIFont.Weight(rawValue: 1.0))
         textfield.adjustsFontSizeToFitWidth = true
         //textfield.borderStyle = UITextField.BorderStyle.roundedRect
         textfield.textAlignment = .center
@@ -79,14 +91,16 @@ public class OnboardingView: UIView {
         textfield.keyboardType = UIKeyboardType.default
         textfield.returnKeyType = UIReturnKeyType.done
         textfield.clearButtonMode = UITextField.ViewMode.whileEditing
+        textfield.autocapitalizationType = .none
         textfield.contentVerticalAlignment = UIControl.ContentVerticalAlignment.center
+        textfield.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
         return textfield
     }()
     
     //Don't know username
     let dontKnowUsernameLabel: UILabel = {
         let label = UILabel()
-        label.font = UIFont.systemFont(ofSize: 50.0)
+        label.font = UIFont.systemFont(ofSize: 25.0)
         label.adjustsFontSizeToFitWidth = true
         label.text = "Don't know your username? Go to your GitHub profile and find it in the URL."
         label.numberOfLines = 0
@@ -98,6 +112,8 @@ public class OnboardingView: UIView {
     //GitHub profile pic
     let githubPhoto: UIImageView = {
         let photo = UIImageView()
+        photo.image = #imageLiteral(resourceName: "Screen Shot 2019-08-11 at 7.24.39 PM")
+        photo.contentMode = .scaleAspectFit
         return photo
     }()
     
