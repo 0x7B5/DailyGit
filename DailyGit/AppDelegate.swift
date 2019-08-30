1273fc4a45466729800180ca147e3ad14c8a395b
Vlad Munteanu <vladcmunteanu@gmail.com>
2019-08-30 13:55:36 -0400

    Added custom alert before notification alert.

--- DailyGit/AppDelegate.swift
+++ DailyGit/AppDelegate.swift
@@ -11,20 +11,24 @@ import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
+    
     
     //Notifications 
     func registerForPushNotifications() {
-      UNUserNotificationCenter.current() // 1
-        .requestAuthorization(options: [.alert, .sound, .badge]) { // 2
-          granted, error in
-         
-      }
+        UNUserNotificationCenter.current()
+            .requestAuthorization(options: [.alert, .sound, .badge]) {
+                [weak self] granted, error in
+                
+                print("Permission granted: \(granted)")
+                guard granted else { return }
+                self?.getNotificationSettings()
+        }
     }
-
+    
     #warning("Debug purposes")
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         UITabBar.appearance().tintColor = Constants.gitGreenColor
         window = UIWindow(frame: UIScreen.main.bounds)
@@ -39,37 +43,80 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             //Not Logged In
             let navController = UINavigationController(rootViewController: OnboardingVC())
             window?.rootViewController = navController
+            let alertController = UIAlertController(title: "Do you want to recieve daily alerts reminding you to commit?", message: "Great! We can notify you.", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "Yes", style: UIAlertAction.Style.default) {
+                UIAlertAction in
+                NSLog("OK Pressed")
+                self.registerForPushNotifications()
+            }
+            let cancelAction = UIAlertAction(title: "No", style: UIAlertAction.Style.cancel) {
+                UIAlertAction in
+                NSLog("Cancel Pressed")
+            }
+            alertController.addAction(okAction)
+            alertController.addAction(cancelAction)
+            self.window?.rootViewController?.present(alertController, animated: true, completion: nil)
         }
         
         
+        #warning("Have to present our own custom alert first here")
         
-        registerForPushNotifications()
         return true
     }
-
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        if(UserDefaults.standard.object(forKey: "CurrentUser") != nil) {
+            ReadUserInfoHelper.shared.refreshEverything(completion: {
+                
+            })
+        }
     }
-
+    
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
     
-
-
+    func getNotificationSettings() {
+        UNUserNotificationCenter.current().getNotificationSettings { settings in
+            print("Notification settings: \(settings)")
+            guard settings.authorizationStatus == .authorized else { return }
+            DispatchQueue.main.async {
+                UIApplication.shared.registerForRemoteNotifications()
+            }
+        }
+    }
+    
+    func application(
+        _ application: UIApplication,
+        didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+    ) {
+        let tokenParts = deviceToken.map { data in String(format: "%02.2hhx", data) }
+        let token = tokenParts.joined()
+        print("Device Token: \(token)")
+    }
+    
+    func application(
+        _ application: UIApplication,
+        didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Failed to register: \(error)")
+    }
+    
+    
+    
 }
 
